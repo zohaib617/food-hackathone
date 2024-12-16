1729abed633cbae6e1bf4f8d2647b775061399cb
syed zohaib shah <160512319+zohaib617@users.noreply.github.com>
2024-12-17 00:32:30 +0500

    Add files via upload

---
+++ src/app/details/page.tsx
@@ -0,0 +1,224 @@
+import Navbar from "@/app/components/navbar";
+import Footer from "@/app/components/footer";
+export default function Details(){
+
+
+
+
+    return(
+
+
+        <>
+        
+        <section className=" bg-white   w-full lg:w-[1920px]" >
+        
+        
+        <div className="bg-[url('/image/menu.png')] bg-cover bg-center w-full h-[250px]  md:h-[350px]  lg:h-[410px] flex justify-center items-center">
+        
+        
+        <div className="text-center">
+
+        <h1 className="text-white  text-[32px] md:text-[48px] lg:text-[56px] font-sans " >Shop</h1>
+        <ul className="text-white flex justify-center space-x-2 mt-2    " >
+            <li><a href="/hero" className=" hover:text-[#ff9f0d] ">home   </a></li>
+            <li className="">&gt;</li>
+            <li><a href="/blog" className="hover:text-[#ff9f0d] ">Shop Details</a></li>
+        </ul>
+        </div>
+        
+        </div>
+        
+
+
+
+
+
+<div className="flex justify-center items-center mt-24" >
+
+
+
+<div className=" grid grid-cols-1 md:grid-cols-2 lg:flex  w-full lg:w-[1320px] lg:h-[718px]  p-4" >
+
+<div className="justify-between p-4">
+    <img src="/suji1.png" alt="food" className="p-3" />
+    <img src="/suji2.png" alt="food" className="p-3" />
+    <img src="/suji3.png" alt="food" className="p-3"/>
+    <img src="/suji4.png" alt="food" className="p-3"/>
+</div>
+
+
+<div className="p-4">
+    <img src="/eggsfood.png" alt="egg" />
+</div>
+
+
+<div className="p-4">
+
+    <div className=" flex justify-between  w-full lg:w-[685px]">
+
+<div className=" flex justify-center items-center w-[86px] h-[28px]  bg-[#FF9F0D] text-white " >In stock</div>
+
+
+<div>
+     <button className="hover:text-white bg-[#ffffff]
+                                    hover:bg-[#FF9F0D] p-2" >  <span className="p-2">&#129032;</span>Prev</button>
+<button className="hover:text-white bg-[#ffffff]
+                                    hover:bg-[#FF9F0D] p-2" >Next<span className="p-2 ">&#129034;</span></button>
+ </div>
+
+    </div>
+<h1 className="  text-[32px] md:text[40px] lg:text-[48px] font-sans font-bold" >Yummy Chicken Chup</h1>
+<p className=" w-ful lg:w-[608px] mt-6">Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+     Quisque diam pellentesque bibendum non dui volutpat fringilla bibendum.
+      Urna, urna, vitae feugiat pretium donec id elementum.
+       Ultrices mattis sed vitae mus risus. Lacus nisi,
+     et ac dapibus sit eu velit in consequat.</p>
+
+<h1 className=" text-[20px] lg:text-[32px] font-bold mt-12" >54.00$</h1>
+
+<div className=" flex  justify-start items-center  mt-4 w-full lg:w-[260px]">
+    
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+                     
+    <span className="fa fa-star text-[#FF9F0D]"></span>
+<span className="fa fa-star text-[#FF9F0D]"></span>
+<span className="fa fa-star text-[#FF9F0D]"></span>
+<span className="fa fa-star text-[#FF9F0D]"></span>
+<span className="fa fa-star text-[#FF9F0D]"></span>
+
+  <p className="" > /  5.0 rating / 22 views</p> 
+</div>
+
+<p className="mt-4" >Dictum/cursus/Risus</p>
+<div className=" flex justify-between items-center  w-full lg:w-[306px] h-[51px] mt-4" >
+<button className="w-[50px] h-[50px]  bg-[#ffffff] hover:bg-[#FF9F0D]" ><span className="text-3xl">-</span></button>
+
+<div className=" flex justify-center items-center text-black text-2xl w-[50px] h-[50px] bg-[#ffffff]  " >1</div>
+
+<button className=" text-black text-2xl w-[50px] h-[50px] bg-[#ffffff]  hover:bg-[#FF9F0D]" >+</button>
+
+<button className=" text-black text-lg w-[191px] h-[50px] bg-[#ffffff]  hover:bg-[#FF9F0D]" >Add to Cart</button>
+
+    </div> 
+
+<div className="text-[#4F4F4F] mt-4 text-lg" >
+    <p>Add to Wishlist Compare</p>
+    <p>Category: Pizza</p>
+    <p>Tag: Our Shop</p>
+    <img src="/social-icon.png" alt="social" className="mt-4" />
+</div>
+</div>
+
+</div>
+
+
+
+
+</div>
+
+
+
+
+<div className="flex justify-center items-center ">
+
+
+
+
+<div className=" w-full lg:w-[1320px] p-6 lg:p-0 ">
+
+    <div className="flex" ><div className=" flex justify-center items-center w-[165px] h-[50px] bg-[#FF9F0D] " >Description</div>
+    <p className="p-4">Reviews (24)</p></div>
+<p>Nam tristique porta ligula, vel viverra sem eleifend nec.
+     Nulla sed purus augue, eu euismod tellus. Nam mattis eros nec mi sagittis sagittis. Vestibulum suscipit cursus bibendum. Integer at justo eget sem auctor auctor eget vitae arcu. Nam tempor malesuada porttitor. Nulla quis dignissim ipsum. Aliquam pulvinar iaculis justo, sit amet interdum sem hendrerit vitae. Vivamus vel erat tortor. Nulla facilisi. In nulla quam,
+     lacinia eu aliquam ac, aliquam in nisl.
+     </p>
+    <p  className="mt-8"> Suspendisse cursus sodales placerat. Morbi eu lacinia ex. Curabitur blandit justo urna,
+      id porttitor est dignissim nec. Pellentesque scelerisque hendrerit posuere. Sed at dolor quis nisi rutrum accumsan et sagittis massa. Aliquam aliquam accumsan lectus quis auctor. Curabitur rutrum massa at volutpat placerat. Duis sagittis vehicula fermentum. Integer eu vulputate justo. Aenean pretium odio vel tempor sodales.
+      Suspendisse eu fringilla leo, non aliquet sem.
+</p>
+
+<p className=" w-full lg:w-[500px] mt-6 "><span className="text-[18px] font-bold font-sans">Key Benefits</span>
+
+<li>Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+</li>
+<li>Maecenas ullamcorper est et massa mattis condimentum.
+</li>
+<li>Vestibulum sed massa vel ipsum imperdiet malesuada id tempus .</li>
+<li>Etiam nec massa et lectus faucibus ornare congue in nunc.</li>
+<li>Mauris eget diam magna, in blandit turpis.</li>
+</p>
+</div>
+
+
+
+</div>
+
+
+
+
+<div className=" flex justify-center items-center  mt-24">
+
+<div className="w-full lg:w-[1320px]  p-6 lg:p-0" >
+
+                            <div className="flex justify-between items-center">
+                                <h1 className="text-[32px] font-sans font-bold">Similar Products</h1>
+                                <div>
+                                    <button><span className="p-2 text-black hover:text-white bg-[#ffffff]
+                                    hover:bg-[#FF9F0D] w-[40px] h-[40px] rounded-full ">&#129032;</span></button>
+                                    <button><span className="p-2 text-black hover:text-white bg-[#ffffff]
+                                    hover:bg-[#FF9F0D] w-[40px] h-[40px] rounded-full ">&#129034;</span></button>
+                                </div>
+
+                            </div>
+
+
+<div className=" grid grid-cols-1 md:grid-cols-2 lg:flex justify-between p-8 lg:p-0 mt-12 ">
+
+<div className="transfrom hover:scale-110 transition duration-500 ease-out"><img src="/mask1.png" alt="food" className="w-[312px] h-[267px] " />
+        <h1 className="text-[18px] font-sans font-bold">Fresh Lime</h1>
+        <h1 className="text-[#FF9F0D] text-[16px] p-2">$38.00  
+            <span className=" text-[16px] text-gray-500 p-2 line-through"> $45.00</span></h1>
+        </div>
+
+
+        <div className="transfrom hover:scale-110 transition duration-500 ease-out"><img src="/mask3.png" alt="food" className="w-[312px] h-[267px] " />
+        <h1 className="text-[18px] font-sans font-bold">Chocolate Muffin</h1>
+        <h1 className="text-[#FF9F0D] text-[16px] p-2">$28.00  
+            </h1>
+        </div>
+
+
+
+        <div className="transfrom hover:scale-110 transition duration-500 ease-out" ><img src="/mask2.png" alt="food" className="w-[312px] h-[267px] " />
+        <h1 className="text-[18px] font-sans font-bold">Burger</h1>
+        <h1 className="text-[#FF9F0D] text-[16px] p-2">$21.00  
+        <span className=" text-[16px] text-gray-500 p-2 line-through"> $45.00</span>
+            </h1>
+        </div>
+
+
+        <div className="transfrom hover:scale-110 transition duration-500 ease-out"><img src="/mask1.png" alt="food" className="w-[312px] h-[267px] " />
+        <h1 className="text-[18px] font-sans font-bold">Fresh Lime</h1>
+        <h1 className="text-[#FF9F0D] text-[16px] p-2">$38.00  
+            <span className=" text-[16px] text-gray-500 p-2 line-through"> $45.00</span></h1>
+        </div>
+
+
+
+
+</div>
+
+</div>
+
+
+</div>
+
+<Footer/>
+
+
+        </section>
+        
+        </>
+    )
+
+}
